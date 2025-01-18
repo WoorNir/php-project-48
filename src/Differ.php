13f5c23f636baad5b130b014d8be229eb00484ed
Egor <woornir@yandex.ru>
2025-01-18 15:14:57 +0300

    refactoring

--- src/Differ.php
+++ src/Differ.php
@@ -6,19 +6,19 @@ function genDiff($first, $second)
 {
     $keys = array_unique(array_merge(array_keys($first), array_keys($second)));
     sort($keys);
-    $result = array_map(function ($key) use ($first, $second) {
+    $result = [];
+    foreach ($keys as $key) {
         if (array_key_exists($key, $first) && array_key_exists($key, $second)) {
             if ($first[$key] === $second[$key]) {
-                return "   $key:" . " $first[$key]";
+                $result[] = "   $key:" . " $first[$key]";
             } else {
-                return " - $key:" . "$first[$key]\n" . " + $key:" . "$second[$key]";
+                $result[] = " - $key:" . "$first[$key]\n" . " + $key:" . "$second[$key]";
             }
         } elseif (array_key_exists($key, $first) && !array_key_exists($key, $second)) {
-            return " - $key:" . "$first[$key]";
+            $result[] = " - $key:" . "$first[$key]";
         } elseif (!array_key_exists($key, $first) && array_key_exists($key, $second)) {
-            return " + $key:" . "$second[$key]";
+            $result[] = " + $key:" . "$second[$key]";
         }
-    }, $keys);
-    $result = "{\n" . implode("\n", $result) . "\n}\n";
-    return $result;
+    }
+    return "{\n" . implode("\n", $result) . "\n}\n";
 }
