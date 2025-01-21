6bb3501e0902e4e40902d69ce59dc4fabac1d4ab
Egor <woornir@yandex.ru>
2025-01-21 18:31:11 +0300

    added tests

---
+++ tests/Test.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Test;
+
+use PHPUnit\Framework\TestCase;
+
+use function Src\Differ\genDiff;
+
+class Test extends TestCase
+{
+    public function testDiff()
+    {
+        $expected = file_get_contents(__DIR__ . "/fixtures/expected.txt");
+        $firstFile = __DIR__ . "/fixtures/file1.json";
+        $secondFile = __DIR__ . "/fixtures/file2.json";
+        $result = genDiff($firstFile, $secondFile);
+        $this->assertEquals($expected, $result);
+
+        $expected = file_get_contents(__DIR__ ."/fixtures/expectedIdentical.txt");
+        $firstFile = __DIR__ . "/fixtures/file1.json";
+        $secondFile = __DIR__ . "/fixtures/file1.json";
+        $result = genDiff($firstFile, $secondFile);
+        $this->assertEquals($expected, $result);
+    }
+}
