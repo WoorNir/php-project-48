4e786948afeebd600be78f728ed90944f21dd74c
Egor <woornir@yandex.ru>
2025-02-04 19:35:38 +0300

    added Exception in parsing

--- src/Parsers.php
+++ src/Parsers.php
@@ -6,13 +6,16 @@ use Symfony\Component\Yaml\Yaml;
 
 function parse($filepath)
 {
+    if (!is_readable($filepath)) {
+        throw new \Exception("Неккоректно задан путь");
+    }
     $content = file_get_contents($filepath);
     switch (pathinfo($filepath, $flags = PATHINFO_EXTENSION)) {
         case 'json':
             return json_decode($content, true);
         case 'yml':
         case 'yaml':
-            return Yaml::parse($content, Yaml::PARSE_OBJECT_FOR_MAP);
+            return Yaml::parse($content);
         default:
             echo "Неподдерживаемый формат файла";
             return null;
