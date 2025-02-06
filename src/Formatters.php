f3983ea503e97c29d94209c6503752995413b31a
Egor <woornir@yandex.ru>
2025-02-06 17:20:58 +0300

    added new formatter

--- src/Formatters.php
+++ src/Formatters.php
@@ -2,11 +2,13 @@
 
 namespace Src\Formatters;
 
+use function Src\Formatters\Plain\getPlain;
 use function Src\Formatters\Stylish\getStylish;
 
 function formatter($diff, $format)
 {
     return match ($format) {
         'stylish' => getStylish($diff),
+        'plain' => getPlain($diff)
     };
 }
--- src/Formatters/Plain.php
+++ src/Formatters/Plain.php
@@ -2,7 +2,48 @@
 
 namespace Src\Formatters\Plain;
 
-function getPlain($diff)
+function getPlain($diff, $path = '')
 {
-    
+    $lines = array_map(function ($node) use ($path) {
+        $property = $path ? "{$path}.{$node['key']}" : $node['key'];
+        echo "Processing: {$property}\n";
+        switch ($node['type']) {
+            case 'nested': 
+                return getPlain($node['children'], $property);
+                break;
+            case 'added' :
+                $value = formatValue($node['newValue']);
+                return "Property '{$property}' was added with value: {$value}";
+                break;
+            case 'removed':
+                $value = formatValue($node['oldValue']);
+                return "Property '{$property}' was removed";
+                break;
+            case 'changed':
+                $oldValue = formatValue($node['oldValue']);
+                $newValue = formatValue($node['newValue']);
+                return "Property '{$property}' was updated. From {$oldValue} to {$newValue}";
+                break;
+            case 'unchanged':
+                return null;
+                break;
+            default:
+                throw new \Exception("Ошибка определения типа узла");
+        };
+    },
+    $diff);
+
+    $lines = array_filter($lines, fn($line) => !is_null($line));
+    return implode("\n", $lines);
+}
+
+function formatValue($value)
+{
+    return match (gettype($value)) {
+        'boolean' => $value ? 'true' : 'false',
+        'array' => "[complex value]",
+        'NULL'=> 'null',
+        'string' => "'{$value}'",
+        default => (string)$value
+    };
 }
