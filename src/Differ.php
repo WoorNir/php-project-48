99d29039983e0b46076ccd6bc50398182c66ef7b
Egor <woornir@yandex.ru>
2025-02-10 11:51:02 +0300

    united splited functions for setting nodes in diff

--- src/Differ.php
+++ src/Differ.php
@@ -19,72 +19,31 @@ function makeDiff(array $first, array $second): array
     $keys = array_unique(array_merge(array_keys($first), array_keys($second)));
     $sortedKeys = sort($keys, fn($key1, $key2) => $key1 <=> $key2);
     return array_map(
-        fn($key) => makeNode($key, $first, $second),
-        $sortedKeys
-    );
+        function($key) use ($first, $second) {
+            if (array_key_exists($key, $first) && array_key_exists($key, $second)) {
+                if (is_array($first[$key]) && is_array($second[$key])) {
+                    $node = setNode('nested', $key, null, null, makeDiff($first[$key], $second[$key]));
+                } elseif ($first[$key] === $second[$key]) {
+                    $node = setNode('unchanged', $key, $first[$key], $second[$key]);
+                } else {
+                    $node = setNode('changed', $key, $first[$key], $second[$key]);
+                }
+            } elseif (array_key_exists($key, $first)) {
+                $node = setNode('removed', $key, $first[$key], null);
+            } else {
+                $node = setNode('added', $key, null, $second[$key]);
+            }
+        return $node;
+        }, $sortedKeys);
 }
 
-function makeNode(string $key, array $first, array $second): array
-{
-    if (array_key_exists($key, $first) && array_key_exists($key, $second)) {
-        return getNodeWithSameKeys($key, $first[$key], $second[$key]);
-    } elseif (array_key_exists($key, $first)) {
-        return getRemovedNode($key, $first[$key]);
-    } elseif (array_key_exists($key, $second)) {
-        return getAddedNode($key, $second[$key]);
-    }
-
-    return [];
-}
-
-function getNodeWithSameKeys(string $key, mixed $firstValue, mixed $secondValue): array
-{
-    if (is_array($firstValue) && is_array($secondValue)) {
-        return [
-            'key' => $key,
-            'type' => 'nested',
-            'children' => makeDiff($firstValue, $secondValue),
-            ];
-    } elseif ($firstValue === $secondValue) {
-        return getUnchangedNode($key, $firstValue);
-    } else {
-        return getChangedNode($key, $firstValue, $secondValue);
-    }
-}
-
-function getUnchangedNode(string $key, mixed $value): array
+function setNode(string $type, string $key, mixed $oldValue, mixed $newValue, array $children = null): array
 {
     return [
+        'type' => $type,
         'key' => $key,
-        'type' => 'unchanged',
-        'unchangedValue' => $value
-    ];
-}
-
-function getChangedNode(string $key, mixed $oldValue, mixed $newValue): array
-{
-    return [
-        'key' => $key,
-        'type' => 'changed',
         'oldValue' => $oldValue,
         'newValue' => $newValue,
-    ];
-}
-
-function getRemovedNode(string $key, mixed $value): array
-{
-    return [
-        'key' => $key,
-        'type' => 'removed',
-        'oldValue' => $value
-    ];
-}
-
-function getAddedNode(string $key, mixed $value): array
-{
-    return [
-        'key' => $key,
-        'type' => 'added',
-        'newValue' => $value
+        'children' => $children
     ];
 }
