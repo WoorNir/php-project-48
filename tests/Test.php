dddae703bf549f39c1f9f44415b038a42d84d04d
Egor <woornir@yandex.ru>
2025-02-13 18:11:53 +0300

    added dataProvider for testing files with diff formats

--- tests/Test.php
+++ tests/Test.php
@@ -2,68 +2,69 @@
 
 namespace Tests\Test;
 
+use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
 use function Differ\Differ\genDiff;
 
 class Test extends TestCase
 {
+    #[DataProvider('addDataProvider')]
+    public function testDefault($firstFile, $secondFile)
+    {
+        $expectedResult = file_get_contents($this->getFixturePath('expected.txt'));
+        $firstFile = $this->getFixturePath($firstFile);
+        $secondFile = $this->getFixturePath($secondFile);
+        $result = genDiff($firstFile, $secondFile);
+        $this->assertEquals($expectedResult, $result);
+    }
+
+    #[DataProvider('addDataProvider')]
+    public function testStylishFormat($firstFile, $secondFile)
+    {
+        $expected = file_get_contents($this->getFixturePath('expected.txt'));
+        $firstFile = $this->getFixturePath($firstFile);
+        $secondFile = $this->getFixturePath($secondFile);
+        $result = genDiff($firstFile, $secondFile, "stylish");
+        $this->assertEquals($expected, $result);
+    }
+
+    #[DataProvider('addDataProvider')]
+    public function testPlainFormat($firstFile, $secondFile)
+    {
+        $expected = file_get_contents($this->getFixturePath('expectedPlain.txt'));
+        $firstFile = $this->getFixturePath($firstFile);
+        $secondFile = $this->getFixturePath($secondFile);
+        $result = genDiff($firstFile, $secondFile, "plain");
+        $this->assertEquals($expected, $result);
+    }
+
+    #[DataProvider('addDataProvider')]
+    public function testJsonFormat($firstFile, $secondFile)
+    {
+        $expected = file_get_contents($this->getFixturePath('expectedJson.txt'));
+        $firstFile = $this->getFixturePath($firstFile);
+        $secondFile = $this->getFixturePath($secondFile);
+        $result = genDiff($firstFile, $secondFile, "json");
+        $this->assertEquals($expected, $result);
+    }
+
     public function getFixturePath($fixtureName)
     {
         return __DIR__ . "/fixtures/" . $fixtureName;
     }
 
-    public function testDefault()
+    public static function addDataProvider()
     {
-        $expected = file_get_contents($this->getFixturePath('expected.txt'));
-        $firstFile = $this->getFixturePath('file1.json');
-        $secondFile = $this->getFixturePath('file2.json');
-        $result = genDiff($firstFile, $secondFile);
-        $this->assertEquals($expected, $result);
-    }
-
-    public function testYmlDiff()
-    {
-        $expected = file_get_contents($this->getFixturePath('expected.txt'));
-        $firstYmlFile = $this->getFixturePath('file1.yml');
-        $secondYmlFile = $this->getFixturePath('file2.yaml');
-        $ymlDiff = genDiff($firstYmlFile, $secondYmlFile);
-        $this->assertEquals($expected, $ymlDiff);
-    }
-
-    public function testYmlJsonDiff()
-    {
-        $expected = file_get_contents($this->getFixturePath('expected.txt'));
-        $jsonFile = $this->getFixturePath('file1.json');
-        $ymlFile =  $this->getFixturePath('file2.yaml');
-        $ymlJsonDiff = genDiff($jsonFile, $ymlFile);
-        $this->assertEquals($expected, $ymlJsonDiff);
-    }
-
-    public function testStylishFormat()
-    {
-        $expected = file_get_contents($this->getFixturePath('expected.txt'));
-        $firstFile = $this->getFixturePath('file1.json');
-        $secondFile = $this->getFixturePath('file2.yaml');
-        $result = genDiff($firstFile, $secondFile, "stylish");
-        $this->assertEquals($expected, $result);
-    }
-
-    public function testPlainFormat()
-    {
-        $expected = file_get_contents(__DIR__ . "/fixtures/expectedPlain.txt");
-        $firstFile = $this->getFixturePath('file1.json');
-        $secondFile = $this->getFixturePath('file2.yaml');
-        $result = genDiff($firstFile, $secondFile, "plain");
-        $this->assertEquals($expected, $result);
-    }
-
-    public function testJsonFormat()
-    {
-        $expected = file_get_contents(__DIR__ . "/fixtures/expectedJson.txt");
-        $firstFile = $this->getFixturePath('file1.json');
-        $secondFile = $this->getFixturePath('file2.yaml');
-        $result = genDiff($firstFile, $secondFile, "json");
-        $this->assertEquals($expected, $result);
+        return [
+            'firstCombination' => [
+                'firstFile' => 'file1.json',
+                'secondFile' => 'file2.yaml',
+            ],
+            'secondCombination' => [
+                'firstFile' => 'file1.yml',
+                'secondFile' => 'file2.json'
+            ]
+        ];
     }
 }
